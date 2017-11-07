be590d84f0b21f86eb1bec9ffa02b2549b84a7bd
jalili.developer@gmail.com <jalili.developer@gmail.com>
2017-11-07 09:27:33 -0800

    asdasd

---
+++ IGN/SqlScript/Utility.sql
@@ -0,0 +1,17 @@
+﻿select * from tblNews n
+--where n.RssID = 22
+order by n.NewsID desc 
+
+
+
+
+
+   select * from tblRss rs
+      where rs.RssID
+	Not in( SELECT DISTINCT  n.RssID FROM tblNews n) 
+
+
+
+
+
+	exec GetAllRssCategory
