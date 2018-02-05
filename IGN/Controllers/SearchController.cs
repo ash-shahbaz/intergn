0074265c6ff6bffce84205d783453f2f727f26fe
ash.shahbaz@yahoo.com <ash.shahbaz@yahoo.com>
2018-02-05 11:36:18 +0330

    asd

--- IGN/Controllers/SearchController.cs
+++ IGN/Controllers/SearchController.cs
@@ -10,18 +10,19 @@ namespace IGN.Controllers
     public class SearchController : Controller
     {
         // GET: Search
-        public List<tblAgahi> Search(SearchItems s)
+        public List<tblAgahi> Search(PublicSearchs item)
         {
-
-
-
-
+           
             return null;
         }
 
-
+        
+        public List<tblAgahi> SearchAmlak(SearchAmlaks item)
+        {
+            return null;
+        }
     }
-    public class SearchItems
+    public class PublicSearchs
     {
 
         public int Category { get; set; }
@@ -31,6 +32,36 @@ namespace IGN.Controllers
         public string KindSelect { get; set; }
         public bool HasImage { get; set; }
         public bool HasFast { get; set; }
+
+    }
+    public class SearchAmlaks
+    {
+        public int Category { get; set; }
+        public string SearchInput { get; set; }
+        public int ShahrSelect { get; set; }
+        public string MahaleSelect { get; set; }
+        public string KindSelect { get; set; }
+        public bool HasImage { get; set; }
+        public bool HasFast { get; set; }
+
+        public int Countryside { get; set; }
+        public int Person { get; set; }
+        public int KindTotalPrice { get; set; }
+        public int UntillTotalPrice { get; set; }
+        public int FromTotalPrice { get; set; }
+        public int UntillArea { get; set; }
+        public int FromArea { get; set; }
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
