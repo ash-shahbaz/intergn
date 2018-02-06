1d552bb64e7427e99c99f57cc0d919adbbec5ed2
ash.shahbaz@yahoo.com <ash.shahbaz@yahoo.com>
2018-02-06 09:57:51 +0330

    fdddddddddddd

--- IGN/Controllers/SearchController.cs
+++ IGN/Controllers/SearchController.cs
@@ -245,6 +245,19 @@ namespace IGN.Controllers
         public bool HasFast { get; set; }
 
 
+        public int KindRentPrice { get; set; }
+        public int UntillRentPrice { get; set; }
+        public int FromRentPrice { get; set; }
+
+        public int KindDepositPrice { get; set; }
+        public int UntillDepositPrice { get; set; }
+        public int FromDepositPrice { get; set; }
+
+        public int UntillArea { get; set; }
+        public int FromArea { get; set; }
+        public int Countryside { get; set; }
+        public int Person { get; set; }
+        public int Kind { get; set; }
 
 
     }
@@ -258,7 +271,14 @@ namespace IGN.Controllers
         public bool HasFast { get; set; }
 
 
-
+        public int AdminDoc { get; set; }
+        public int Person { get; set; }
+        public int Kind { get; set; }
+        public int KindTotalPrice { get; set; }
+        public int UntillTotalPrice { get; set; }
+        public int FromTotalPrice { get; set; }
+        public int UntillArea { get; set; }
+        public int FromArea { get; set; }
 
     }
     public class SearchRentOffices
@@ -270,6 +290,18 @@ namespace IGN.Controllers
         public bool HasImage { get; set; }
         public bool HasFast { get; set; }
 
+
+        public int KindRentPrice { get; set; }
+        public int UntillRentPrice { get; set; }
+        public int FromRentPrice { get; set; }
+        public int KindDepositPrice { get; set; }
+        public int UntillDepositPrice { get; set; }
+        public int FromDepositPrice { get; set; }
+        public int UntillArea { get; set; }
+        public int FromArea { get; set; }
+        public int Countryside { get; set; }
+        public int Person { get; set; }
+        public int Kind { get; set; }
 
 
 
@@ -283,7 +315,7 @@ namespace IGN.Controllers
         public bool HasImage { get; set; }
         public bool HasFast { get; set; }
 
-
+        public int Person { get; set; }
 
 
     }
@@ -298,6 +330,46 @@ namespace IGN.Controllers
 
 
 
+        public int KindTotalPrice { get; set; }
+        public int UntillTotalPrice { get; set; }
+        public int FromTotalPrice { get; set; }
+        public int RoomNumber { get; set; }
+        public int UntillArea { get; set; }
+        public int FromArea { get; set; }
+        public int Countryside { get; set; }
+        public int Person { get; set; }
+        public int Kind { get; set; }
+        public int UntillInfrastructure { get; set; }
+        public int FromInfrastructure { get; set; }
+        public int UntillLoan { get; set; }
+        public int FromLoan { get; set; }
+        public int UntillYear { get; set; }
+        public int FromYear { get; set; }
+        public int FloorNumber { get; set; }
+        public int KindBottom { get; set; }
+        public int KindHeating { get; set; }
+
+        public bool HasParking { get; set; }
+        public bool HasElevator { get; set; }
+        public bool HasIranianService { get; set; }
+        public bool HasForeignService { get; set; }
+        public bool HasBalcony { get; set; }
+        public bool HasPool { get; set; }
+        public bool HasUnderground { get; set; }
+        public bool HasYard { get; set; }
+        public bool HasSauna { get; set; }
+        public bool HasJacuzzi { get; set; }
+        public bool HasSleepMaster { get; set; }
+        public bool HasRoofGarden { get; set; }
+        public bool HasChiller { get; set; }
+        public bool HasBarbecue { get; set; }
+        public bool HasAltar { get; set; }
+        public bool HasLobby { get; set; }
+        public bool HasBabyPlayground { get; set; }
+        public bool HasGym { get; set; }
+        public bool HasHeir { get; set; }
+        public bool HasSingleDocument { get; set; }
+        public bool HasLoan { get; set; }
 
     }
     public class SearchSaleLandSubs
