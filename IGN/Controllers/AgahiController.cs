0e25314ca2187ea6de1f4887832e675fb111eedc
jalili.developer@gmail.com <jalili.developer@gmail.com>
2018-02-06 13:15:35 -0800

    asdadasasdasd

--- IGN/Controllers/AgahiController.cs
+++ IGN/Controllers/AgahiController.cs
@@ -1,10 +1,8 @@
 ﻿using IGN.Models;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Web;
-using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace IGN.Controllers
@@ -17,7 +15,7 @@ namespace IGN.Controllers
             return View();
         }
         [ActionName("ثبت-آگهی")]
-        public ActionResult ثبت_آگهی(string name, string id1,string id2, string id3)
+        public ActionResult ثبت_آگهی(string name, string id1, string id2, string id3)
         {
             if (name != "")
             {
@@ -32,7 +30,7 @@ namespace IGN.Controllers
             return View();
         }
 
-     
+
 
 
         [ActionName("جزئیات-آگهی")]
@@ -53,10 +51,10 @@ namespace IGN.Controllers
         {
             return View();
         }
-      
-       
+
+
         [ActionName("جستجو")]
-        public ActionResult جستجو(string name, string id1, string id2,string id3)
+        public ActionResult جستجو(string name, string id1, string id2, string id3)
         {
 
             if (name != "" && name != null)
@@ -64,7 +62,7 @@ namespace IGN.Controllers
                 var qGetProvince = Utility.lstProvinces.Where(p => p.ProvinceName == name).FirstOrDefault();
                 Utility.PrivinceID = qGetProvince.ProvinceID;
 
-               Utility.CityName = name;
+                Utility.CityName = name;
 
                 if (id1 == null && id2 == null && id3 == null)
                 {
@@ -97,30 +95,54 @@ namespace IGN.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public JsonResult AgahiAdd(tblAgahi tagahi)
-        //{
 
-
-        //    Utility.AddAgahi(tagahi);
-
-        //    return Json("Ok");
-
-        //}
-
-        public JsonResult AgahiAdd(tblAgahi tagahi)
+        public JsonResult CaseOne(CaseOnes item)
         {
 
 
-            Utility.AddAgahi(tagahi);
 
             return null;
         }
+    }
 
-        
 
+    public class CaseOnes
+    {
+        //Fixed
+        public int lv0 { get; set; }
+        public int lv1 { get; set; }
+        public int lv2 { get; set; }
+        public int Country { get; set; }
+        public int City { get; set; }
+        public int Region { get; set; }
+        public string Location { get; set; }
+        public int Kind { get; set; }
+        public string TitleAgahi { get; set; }
+        public string uploadimageone { get; set; }
+        public string uploadimagetwo { get; set; }
+        public string uploadimagethree { get; set; }
+        public string uploadimagefour { get; set; }
+        public string txtDesc { get; set; }
+
+        public string NameAdvertiser { get; set; }
+        public string EmailAdvertiser { get; set; }
+        public int MobileAdvertiser { get; set; }
+        public int KindAds { get; set; }
+
+        public string RegisterDate { get; set; }
+        public int UserID { get; set; }
+        public int LanguageID { get; set; }
+        public int Chatable { get; set; }
+        public string OnTime { get; set; }
+        public int SpecialAgahi { get; set; }
+
+        //Internal
 
 
     }
-   
+
+
+
+
+
 }
