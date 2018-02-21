bc10abf914e85359293c38bf5f42e711e213627e
jalili.developer@gmail.com <jalili.developer@gmail.com>
2018-02-21 08:17:16 -0800

    asdad

--- IGN/Models/Search/SearchSaleOffices.cs
+++ IGN/Models/Search/SearchSaleOffices.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace iGNProject.Models.Search
+namespace IGN.Models.Search
 {
     public class SearchSaleOffices
     {
