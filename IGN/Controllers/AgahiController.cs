de6ecbb6848acf0ebed0f561e531cd0d1948319b
ash.shahbaz@yahoo.com <ash.shahbaz@yahoo.com>
2018-07-21 13:24:40 +0430

    asd

--- IGN/Controllers/AgahiController.cs
+++ IGN/Controllers/AgahiController.cs
@@ -143,85 +143,87 @@ namespace IGN.Controllers
         public JsonResult RegisterSaleApartSub(RegisterSaleApartSubs item)
         {
 
+            string result = PostAgahi.AddRegisterSaleApartSubs(item);
+
             return null;
         }
 
         public JsonResult RegisterSaleLandSub(RegisterSaleLandSubs item)
         {
-
+            string result = PostAgahi.AddRegisterSaleLandSubs(item);
             return null;
         }
 
 
         public JsonResult RegisterRentHouseSub(RegisterRentHouseSubs item)
         {
-
+            string result = PostAgahi.AddRegisterRentHouseSubs(item);
             return null;
         }
 
         public JsonResult RegisterSaleOfficeSub(RegisterSaleOfficeSubs item)
         {
-
+            string result = PostAgahi.AddRegisterSaleOfficeSubs(item);
             return null;
         }
 
         public JsonResult RegisterRentOfficeSub(RegisterRentOfficeSubs item)
         {
-
+            string result = PostAgahi.AddRegisterRentOfficeSubs(item);
             return null;
         }
 
         public JsonResult RegisterServicesHouseSub(RegisterServicesHouseSubs item)
         {
-
+            string result = PostAgahi.AddRegisterServicesHouseSubs(item);
             return null;
         }
 
         public JsonResult RegisterCarRiding(RegisterCarRidings item)
         {
-
+            string result = PostAgahi.AddRegisterCarRidings(item);
             return null;
         }
 
         public JsonResult RegisterCarHeavy(RegisterCarHeavys item)
         {
-
+            string result = PostAgahi.AddRegisterCarHeavys(item);
             return null;
         }
 
         public JsonResult RegisterMotorcycle(RegisterMotorcycles item)
         {
-
+            string result = PostAgahi.AddRegisterMotorcycles(item);
             return null;
         }
 
         public JsonResult RegisterAccessoriesDetail(RegisterAccessoriesDetails item)
         {
-
+            string result = PostAgahi.AddRegisterAccessoriesDetails(item);
             return null;
         }
 
         public JsonResult RegisterMobileTablet(RegisterMobileTablets item)
         {
-
+            string result = PostAgahi.AddRegisterMobileTablets(item);
             return null;
         }
 
         public JsonResult RegisterLapTop(RegisterLapTops item)
         {
-
+            string result = PostAgahi.AddRegisterLapTops(item);
             return null;
         }
 
         public JsonResult RegisterCase(RegisterCases item)
         {
-
+            string result = PostAgahi.AddRegisterCases(item);
             return null;
         }
 
         public JsonResult RegisterElectronics(RegisterElectronicss item)
         {
-
+            string result = PostAgahi.AddRegisterElectronicss(item);
             return null;
         }
 
@@ -230,15 +232,12 @@ namespace IGN.Controllers
 
 
             string result = PostAgahi.AddRegisterPersonalEntertainments(item);
-
-
-            
             return null;
         }
 
         public JsonResult RegisterServicesEmployment(RegisterServicesEmployments item)
         {
-
+            string result = PostAgahi.AddRegisterServicesEmployments(item);
             return null;
         }
 
